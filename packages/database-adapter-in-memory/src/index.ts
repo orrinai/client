9a405bdacd24458473cee48bd8e952781a4616c8
orrin-nay <naylor.orrin@gmail.com>
2025-04-07 17:47:45 +0000

    Add start script to cli example

--- packages/database-adapter-in-memory/src/index.ts
+++ packages/database-adapter-in-memory/src/index.ts
@@ -3,3 +3,4 @@ import { InMemoryDatabaseAdapter } from './in-memory-database-adapter.js';
 // Re-export the adapter as both named and default export
 export { InMemoryDatabaseAdapter };
 export default InMemoryDatabaseAdapter;
+
